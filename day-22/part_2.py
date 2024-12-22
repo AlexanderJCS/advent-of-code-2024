12d60499b9d1f3fa56e6fda7e27c7d59908b9a0b
Alex <alexandercastronovo@gmail.com>
2024-12-22 13:26:57 -0500

    day 22 part 2

--- day-22/part_2.py
+++ day-22/part_2.py
@@ -1,38 +1,11 @@
 import itertools
 import time
+from collections import defaultdict, deque
 
 import part_1
 
 
-def score(banana_delta_values, sequence: list[int]):
-    total = 0
-    
-    for banana_delta in banana_delta_values:
-        matches_sequence = 0
-        found = False
-        
-        for delta, value in banana_delta:
-            if delta == sequence[matches_sequence]:
-                matches_sequence += 1
-                
-                if matches_sequence == len(sequence):
-                    found = True
-                    break
-                
-            else:
-                matches_sequence = 0
-        
-        if found:
-            total += value
-    
-    return total
-
-
-def delta_value_pair(li):
-    return [((b - a), b) for a, b in zip(li, li[1:])]
-
-
-def bananas_list(seed):
+def sell_prices(seed):
     bananas = []
     
     for _ in range(2000):
@@ -44,23 +17,25 @@ def bananas_list(seed):
 
 def main():
     seeds = part_1.parse_input()
-    bananas = [bananas_list(seed) for seed in seeds]
-    banana_delta_values = [delta_value_pair(banana_list) for banana_list in bananas]
-
-    max_sequence = []
-    max_score = -1
+    sell_prices_list = [sell_prices(seed) for seed in seeds]
     
-    for i, sequence in enumerate(itertools.product(range(-9, 10), repeat=4)):
-        if i % 1000 == 0:
-            print(time.time(), f"{i / 19 ** 4 * 100}%")
+    sequence_price = defaultdict(int)
+    
+    for prices in sell_prices_list:
+        seen = set()
         
-        seq_score = score(banana_delta_values, sequence)
-        
-        if seq_score > max_score:
-            max_score = seq_score
-            max_sequence = sequence
-        
-    print(max_score, max_sequence)
+        for a, b, c, d, e in zip(prices, prices[1:], prices[2:], prices[3:], prices[4:]):
+            sequence = (b - a, c - b, d - c, e - d)
+            
+            if sequence in seen:
+                continue
+            
+            seen.add(sequence)
+            sequence_price[sequence] += e
+    
+    seq_price_sorted = sorted(sequence_price.items(), key=lambda n: n[1], reverse=True)
+    print(seq_price_sorted[0][0])
+    print(seq_price_sorted[0][1])
 
 
 if __name__ == "__main__":
